dbfa163a780ccd1f793de081470e6fbe45da3aab
David Gaspar <davidgaspar.dev@gmail.com>
2022-11-08 22:21:49 -0300

    chore: create node structure

--- main.c
+++ main.c
@@ -1,4 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+    int value;
+    struct node * left;
+    struct node * right;
+} node;
+
+// Create a tree node
+node* create_node(int value) {
+    node* result = malloc(sizeof(node));
+
+    if(result != NULL) {
+        result->value = value;
+        result->left = NULL;
+        result->right = NULL;
+    }
+
+    return result;
+}
 
 int main() {
     printf("Hello, World!\n");
